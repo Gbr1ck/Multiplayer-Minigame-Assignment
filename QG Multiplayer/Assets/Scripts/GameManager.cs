5d93c509558bf5f91240c6e74073bb292e9f91e7
Leafygreens97 <kshx5@yahoo.com>
2020-11-18 21:02:23 -0700

    Fully integrated multiplayer
    No testing done yet, but the code SHOULD allow for connecting and instantiating of multiple players

---
+++ QG Multiplayer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+    
+    void Start()
+    {
+        PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(-16.94f, 9f, 4.34f), Quaternion.identity, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- QG Multiplayer/Assets/Scripts/Player.cs
+++ QG Multiplayer/Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviourPunCallbacks {
     public float ms = 10;
     public float jumpForce = 10;
     public float sprintMulty = 1.5f;
     private bool grounded = false;
     public GameObject cameraObj;
+    Camera mainCamera;
+    public Camera playerCamera;
     private Vector3 cameraDelta = new Vector3 ();
     Rigidbody rb;
 
-    void Start () => rb = this.GetComponent<Rigidbody> ();
-
+    void Start (){
+        rb = this.GetComponent<Rigidbody> ();
+        mainCamera = Camera.main;
+        if (photonView.IsMine){
+            mainCamera.enabled = false;
+            playerCamera.enabled = true;
+        }
+    }
     void Update () {
         if (Input.GetMouseButton (0) || Input.GetMouseButton (1) || Input.GetMouseButton (2)) Cursor.lockState = CursorLockMode.Locked;
         else if (Input.GetButtonDown ("Cancel")) Cursor.lockState = CursorLockMode.None;
