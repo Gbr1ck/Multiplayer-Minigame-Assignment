099057c97ec8da33e4ea50eb5457479d5e04cc28
Leafygreens97 <kshx5@yahoo.com>
2020-11-20 14:16:26 -0700

    Added some tag functionality

--- QG Multiplayer/Assets/Scripts/GameManager.cs
+++ QG Multiplayer/Assets/Scripts/GameManager.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
+    public Text timer;
+    float roundTime = 180.0f;
     
     void Start()
     {
         PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(-16.94f, 9f, 4.34f), Quaternion.identity, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        roundTime -= Time.deltaTime;
+        timer.text = "Time Left: " + roundTime.ToString();
     }
 }
---
+++ QG Multiplayer/Assets/Scripts/Tag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Tag : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+    public Material taggedMaterial;
+    public Material defaultMaterial;
+    [SerializeField]
+    bool isTagged = false;
+    
+    void OnTriggerEnter(Collider other){
+        if (photonView.IsMine == false){
+            return;
+        }
+        else if (other.tag == "Player"){
+            if (isTagged == true){
+                isTagged = false;
+                player.GetComponent<MeshRenderer>().material = defaultMaterial;
+            }
+            else if (isTagged == false){
+                isTagged = true;
+                player.GetComponent<MeshRenderer>().material = taggedMaterial;
+            }
+        }
+    }
+}
