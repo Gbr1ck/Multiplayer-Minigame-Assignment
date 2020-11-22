f9267c0b37132e47e2c7429d68dce7f681a7672f
Leafygreens97 <kshx5@yahoo.com>
2020-11-22 14:53:48 -0700

    More Tag fixes

--- QG Multiplayer/Assets/Scripts/Tag.cs
+++ QG Multiplayer/Assets/Scripts/Tag.cs
@@ -6,37 +6,40 @@ using Photon.Pun;
 public class Tag : MonoBehaviourPun
 {
     public GameObject player;
-    public Color taggedColor = Color.yellow;
-    public Color defaultColor = Color.gray;
+    public Material taggedMaterial;
+    public Material defaultMaterial;
 
     [PunRPC]
     void TaggedColor(){
-        player.GetComponent<MeshRenderer>().material.color = taggedColor;
+        player.GetComponent<MeshRenderer>().material = taggedMaterial;
+        player.tag = "TaggedPlayer";
     }
     [PunRPC]
     void DefaultColor(){
-        player.GetComponent<MeshRenderer>().material.color = defaultColor;
+        player.GetComponent<MeshRenderer>().material = defaultMaterial;
+        player.tag = "Player";
     }
 
     void Start(){
-        if (photonView.IsMine){
+        if (photonView.IsMine && player.tag == "Player"){
             this.photonView.RPC("DefaultColor", RpcTarget.All);
+        }
+        else if (photonView.IsMine && player.tag == "TaggedPlayer"){
+            this.photonView.RPC("TaggedColor", RpcTarget.All);
         }
     }
     
     void OnTriggerEnter(Collider other){
-        if (photonView.IsMine == false){
-            return;
-        }
         //getting tagged by another player
-        else if (other.tag == "TaggedPlayer" && player.tag == "Player"){
+        if (other.tag == "TaggedPlayer" && player.tag == "Player"){
             this.photonView.RPC("TaggedColor", RpcTarget.All);
-            player.tag = "TaggedPlayer";
+            Debug.Log("I've been tagged");
         }
         //tagging another player
         else if (other.tag == "Player" && player.tag == "TaggedPlayer"){
             this.photonView.RPC("DefaultColor", RpcTarget.All);
-            player.tag = "Player";
+            Debug.Log("I tagged someone");
         }
     }
 }
+
